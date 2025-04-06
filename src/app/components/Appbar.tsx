515cb41c8b4ec7f330ce046013528387b8c37aa0
Aniruddh-Semalty <semaltyaniruddh001@gmail.com>
2025-04-06 18:33:42 +0530

    Added shadcn for components

--- src/app/components/Appbar.tsx
+++ src/app/components/Appbar.tsx
@@ -1,12 +1,32 @@
 "use client";
-import { signIn, useSession ,signOut} from "next-auth/react"
-export default  function Appbar(){
-    const session=useSession();
-    console.log(session);
-    return <div className="flex justify-between">
-        <div>Listenify</div><div>
-           {!session.data?.user && <button  className="m-2 p-2 bg-blue-400" onClick={()=>signIn()}>Sign in</button>}
-           {session.data?.user && <button  className="m-2 p-2 bg-blue-400" onClick={()=>signOut()}>Sign Out</button>}
-        </div>
+import { signIn, useSession, signOut } from "next-auth/react";
+import { Button } from "@/components/ui/button";
+export default function Appbar() {
+  const session = useSession();
+  console.log(session);
+  return (
+    <div className="flex justify-between text-white font-bold text-xl p-8 items-center">
+      <div className="flex flex-col justify-center">
+      Listenify
+      </div>
+      <div className="flex flex-col justify-center">
+        {!session.data?.user && (
+          <Button
+            className="bg-purple-600 text-white hover:bg-purple-700"
+            onClick={() => signIn()}
+          >
+            Sign in
+          </Button>
+        )}
+        {session.data?.user && (
+          <Button
+            className="bg-purple-600 text-white hover:bg-purple-700"
+            onClick={() => signOut()}
+          >
+            Sign Out
+          </Button>
+        )}
+      </div>
     </div>
+  );
 }
